17e2212d3a710520bdc67cb1d9f8c2ccee9dd539
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-24 00:52:36 -0300

    create product find all paged

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscommerce.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,12 @@ public class ProductService {
   public ProductDTO findById(Long id) {
     Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException());
     return new ProductDTO(product);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<ProductDTO> findAll(Pageable pageable) {
+    Page<Product> products = productRepository.findAll(pageable);
+    return products.map(entity -> new ProductDTO(entity));
   }
 
 }
