2761cdfa297ff4cf042aeb36fb700f742211cbd5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-01-15 00:26:46 -0300

    Controle de acesso por rota

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,6 +44,7 @@ public class ProductController {
     return ResponseEntity.ok(dto);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
   @PostMapping
   public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
     dto = productService.insert(dto);
@@ -54,12 +56,14 @@ public class ProductController {
     return ResponseEntity.created(uri).body(dto);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
   @PutMapping(path = "/{id}")
   public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
     dto = productService.update(id, dto);
     return ResponseEntity.ok(dto);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
   @DeleteMapping(path = "/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     productService.delete(id);
